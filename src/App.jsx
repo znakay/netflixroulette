c6f0e3b4c8ad9e4b8c262bcc25e391d47ad8f083
Andrey Znak <andreyznak@estr.co>
2023-12-21 22:41:07 +0300

    feat: add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <div className="wrapper">
-
+        <Counter />
       </div>
     </div>
   );
---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./index.scss";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  createLayout = () => {
+    const counterIncrement = React.createElement(
+      "button",
+      {
+        className: "counter__button counter__button_increment",
+        onClick: this.increment,
+      },
+      "+"
+    );
+    const counterDecrement = React.createElement(
+      "button",
+      {
+        className: "counter__button counter__button_decrement",
+        onClick: this.decrement,
+      },
+      "-"
+    );
+    const counterInput = React.createElement("input", {
+      className: "counter__input",
+      value: this.state.count,
+    });
+
+    return React.createElement("div", { className: "counter" }, [
+      counterDecrement,
+      counterInput,
+      counterIncrement,
+    ]);
+  };
+
+  render() {
+    return this.createLayout();
+  }
+}
+
+export default Counter;
